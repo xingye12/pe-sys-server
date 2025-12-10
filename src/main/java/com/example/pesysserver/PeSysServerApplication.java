fdf6b39c107007ae9addf49b1f1ef198f2e8b828
86198 <2337759353@qq.com>
2025-12-10 09:43:53 +0800

    first

---
+++ src/main/java/com/example/pesysserver/PeSysServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.pesysserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeSysServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeSysServerApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/pesysserver/service/AdminService.java
@@ -0,0 +1,4 @@
+package com.example.pesysserver.service;
+
+public interface AdminService {
+}
